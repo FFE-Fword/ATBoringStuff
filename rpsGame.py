55d7835ec0993d37abc563e1a88692d3634a74af
Dkass <d.kassaye@outlook.com>
2019-11-27 20:58:18 -0600

    Update rpsGame.py

--- rpsGame.py
+++ rpsGame.py
@@ -15,4 +15,23 @@ while True: # The main game loop
         if playerMove == "r" or playerMove == "p" or playerMove == "s":
             break # Break out of the player input loop
         print("Type one of r, p, s, or q")
-        
+
+    #Display what the player chose:
+    if playerMove =="r":
+        print("ROCK versus...")
+    elif playerMove == "p":
+        print("PAPER versus...")
+    elif playerMove == "s":
+        print("SCISSORS versus...")
+    
+    #Display what the computer chose:
+    randomNumber = random.randint(1,3)
+    if randomNumber == 1:
+        computerMove = "r"
+        print("ROCK")
+    elif randomNumber == 2:
+        computerMove == "p"
+        print("PAPER")
+    elif randomNumber == 3:
+        computerMove == "s"
+        print("SCISSORS")
